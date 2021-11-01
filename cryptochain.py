776272cfc7d288b78b5ef1911526ec5a025831fa
Edgard Lopes <70863504+EdLLopes@users.noreply.github.com>
2021-11-01 13:23:00 -0300

    Update cryptochain.py

--- cryptochain.py
+++ cryptochain.py
@@ -1,9 +1,3 @@
-""" Importante entender que esse codigo é um demonstrativo para pessoas leigas compreenderem como funciona um blockchain.
-Um sistema real teria criptografias mais robustas, reconheceria cada player desse mercado, e como cada player tem sua blockchain verificaria antes de uma transacao se o hash e valido. (Todos players devem ter o mesmo hash para participar do sistema, ja que tem a mesma chain).
-O sistema de Proof odf Work e Mining, estão super simplificados perto de um codigo real, porem nesse e possivel perceber o sistema de segurança do blockchain, basta alterar o parametro difficuty do blockchain e vera a dificuldade do seu processador de realizar o PoW. 
-"""
-
-
 
 from hashlib import sha256 # Algoritmo de hash seguro de 256 bits
 
